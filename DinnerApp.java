b48eb072eb4b260e886d173ad688848e585e9813
KvnAuer <kza0067@auburn.edu>
2021-01-06 20:24:57 -0600

    setting up api connection. Will have to finish tomorrow

--- DinnerApp.java
+++ DinnerApp.java
@@ -1,11 +1,40 @@
-
-
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class DinnerApp {
 
     public void main(String args[]) {
 
+
+        try {
             
+            BufferedReader reader = new BufferedReader(System.in);
+            System.out.println("Please enter cuisine type [African, American, British,
+                Cajun, Caribbean, Chinese, Eastern European, European, French, German,
+                Greek, ]")
+
+
+            URL url = new URL("myURLgoesHere");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            //
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            int status = connection.getResponseCode();
+
+            System.out.println(status);
+
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+       
+
+        //
+
+
 
     }
 }
