b765343c38397122b22a70575b0d01fd6bb2f6b3
KvnAuer <kza0067@auburn.edu>
2021-01-07 20:54:33 -0600

    Added ingredient class and added code to recipe class

---
+++ Ingredient.java
@@ -0,0 +1,13 @@
+public class Ingredient {
+    String name, amount, unit;
+    
+    public Ingredient(String nameIn, String amountIn, String unitIn) {
+
+        name = nameIn;
+        amount = amountIn;
+        unit = unitIn;
+
+
+    }
+
+}
--- Recipe.java
+++ Recipe.java
@@ -1,11 +1,16 @@
-
-
+import java.util.ArrayList;
 
 public class Recipe {
 
-    public Recipe() {
+    String name, summary, instructions;
+    ArrayList<Ingredient> ingredientList;
 
-        
+    public Recipe(String nameIn, String summaryIn, ArrayList<Ingredient> ingredientListIn, String instructionsIn) {
+
+        name = nameIn;
+        summary = summaryIn;
+        ingredientList = ingredientListIn;
+        instructions = instructionsIn;
 
     }
 
