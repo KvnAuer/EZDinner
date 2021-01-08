96a4a4b730832bcafad85fdf23f821ba1926d8f8
KvnAuer <kza0067@auburn.edu>
2021-01-07 20:30:09 -0600

    added user inputs to make API request

--- DinnerApp.java
+++ DinnerApp.java
@@ -1,20 +1,37 @@
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+
+
+import java.io.*;
+
 public class DinnerApp {
 
-    public void main(String args[]) {
+    public static void main(String args[]) {
 
 
         try {
             
-            BufferedReader reader = new BufferedReader(System.in);
-            System.out.println("Please enter cuisine type [African, American, British,
-                Cajun, Caribbean, Chinese, Eastern European, European, French, German,
-                Greek, ]")
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Please enter cuisine type [African, American, British,"
+                + "Cajun, Caribbean, Chinese, Eastern European, European, French, German,"
+                + "Greek, Indian, Irish, Italian, Japanese, Jewish, Korean, Latin American,"
+                + "Mediterranean, Mexican, Middle Eastern, Nordic, Southern, Spanish, Thai, Vietnamese]");
 
+            String cuisine = reader.readLine();
 
-            URL url = new URL("myURLgoesHere");
+            System.out.println("main course, side dish, dessert, appetizer, breakfast, soup, beverage, sauce, marinade, finger food, snack, drink");
+
+            String type = reader.readLine();
+
+            String myUrl = "https://api.spoonacular.com/recipes/random?"
+            + "cuisine=" + cuisine
+            + "&type=" + type
+            + "&number=1&apiKey=" + Config.spoonacularAPIKey;
+
+            System.out.println(myUrl.replaceAll(" ", ""));
+
+            URL url = new URL(myUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
             //
@@ -23,6 +40,12 @@ public class DinnerApp {
             connection.setReadTimeout(5000);
 
             int status = connection.getResponseCode();
+
+            String contentType = connection.getContentType();
+
+            System.out.println(contentType);
+
+            //connection.getContent();
 
             System.out.println(status);
 
---
+++ Recipe.java
@@ -0,0 +1,12 @@
+
+
+
+public class Recipe {
+
+    public Recipe() {
+
+        
+
+    }
+
+}
